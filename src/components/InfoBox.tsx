344acc1fc90c8aa27b55ee84b167332db3310ffd
markusoxnevad1 <markus.oxnevad@gmail.com>
2024-10-17 20:26:09 +0200

    feat: randomize winner sounds

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
 import TimeUntilNextDay from "./TimeUntilNextGame";
 import PlaySound from "./PlaySound";
-import sound from "../../public/sounds/winner.wav";
+import sound1 from "../../public/sounds/winner.wav";
+import sound2 from "../../public/sounds/big_bear.wav";
+import sound3 from "../../public/sounds/cherry_popper.wav";
+import sound4 from "../../public/sounds/diva.wav";
+import sound5 from "../../public/sounds/domination.wav";
+import sound6 from "../../public/sounds/fabulous.wav";
+import sound7 from "../../public/sounds/homewrecker.wav";
+import sound8 from "../../public/sounds/homicidal.wav";
+import sound9 from "../../public/sounds/matcho.wav";
+import sound10 from "../../public/sounds/rainbow_warrior.wav";
+import sound11 from "../../public/sounds/savage_ouh.wav";
+import sound12 from "../../public/sounds/superstar.wav";
+import sound13 from "../../public/sounds/unicorn_stampeede.wav";
+
 
 const InfoBox = ({ guesses, message, finished }:{
     guesses?: number;
@@ -11,9 +24,29 @@ const InfoBox = ({ guesses, message, finished }:{
 }) => {
 
     const [playSound, setPlaySound] = useState<boolean>(false);
+    const [selectedSound, setSelectedSound] = useState<string | null>(null);
+
 
     useEffect(() => {
         if (finished) {
+            setPlaySound(true);
+            const sounds = [
+                sound1,
+                sound2,
+                sound3,
+                sound4,
+                sound5,
+                sound6,
+                sound7,
+                sound8,
+                sound9,
+                sound10,
+                sound11,
+                sound12,
+                sound13
+            ];
+            const randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+            setSelectedSound(randomSound);
             setPlaySound(true);
         }
     }, [finished]);
@@ -26,7 +59,7 @@ const InfoBox = ({ guesses, message, finished }:{
                 <>
                     <div style={{ color: "#24d475" }}>Congratulations! You've won!</div>
                     <TimeUntilNextDay />
-                    {playSound && <PlaySound fileName={sound} />}
+                    {playSound && <PlaySound fileName={selectedSound ? selectedSound : sound1} />}
                 </>
             )}
         </div>
